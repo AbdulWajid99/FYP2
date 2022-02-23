cbd7359c254d40da0e75f4c0dfbd7038f90397e8
AbdulWajid99 <wajid.linux99@gmail.com>
2022-02-23 09:38:04 +0500

    Finalized

--- app.py
+++ app.py
@@ -6,12 +6,11 @@ import shutil
 
 app = FastAPI()
 
-lower_green = np.array([36 ,0 , 0])
-upper_green = np.array([102,255,255])
 
-@app.get("/index")
+
+@app.get("/")
 def index():
-    return "Welcome to PAKPLANTS"
+    return{"Welcome to PAKPLANTS"}
 
 @app.post("/percent")
 def mask(file: UploadFile =File(...)):
@@ -21,9 +20,11 @@ def mask(file: UploadFile =File(...)):
     img= cv2.imread(file.filename) 
     grid_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     grid_HSV = cv2.cvtColor(grid_RGB, cv2.COLOR_RGB2HSV)
+    lower_green = np.array([36 ,0 , 0])
+    upper_green = np.array([102,255,255])
     mask= cv2.inRange(grid_HSV, lower_green, upper_green)
     green_perc = (np.sum(mask) / np.size(mask))/255
     green_perc = green_perc*100
-    return round(green_perc,3)
+    return{round(green_perc,3)}
 
 
