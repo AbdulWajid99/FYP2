d3024e68a90b532ae78da4e42190a4f6af2d23b2
AbdulWajid99 <wajid.linux99@gmail.com>
2022-02-27 00:41:18 +0500

    Re-deploy

--- app.py
+++ app.py
@@ -6,11 +6,10 @@ import shutil
 import tensorflow
 from PIL import Image
 from tensorflow.keras.preprocessing import image
-import os
+
 
 # model import 
-model_path = os.path.join("","model_final.h5")
-model = tensorflow.keras.models.load_model(model_path)
+model = tensorflow.keras.models.load_model("model_final.h5")
 # classes label
 index_to_label = {
     0: "Alstonia Scholaris",
